7f64f452087ca47cac4c2e02d6714c1263a04a3f
Jake Probst <jake.probst@gmail.com>
2014-05-29 01:05:00 -0700

    sentence/parsing test code

--- src/iplus1d/main.c
+++ src/iplus1d/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "iplus1.h"
 #include "cache.h"
-
+#include "sentence.h"
 
 
 
@@ -10,6 +12,7 @@
 int main()
 {
     iplus1_t iplus1;
+    iplus1_sentence_t sen;
     //iplus1_cache_t cache;
     
     //iplus1_cache_init(&cache, NULL);
@@ -17,13 +20,22 @@ int main()
     
     iplus1_init(&iplus1);
     
-    //iplus1_set_cache(&iplus1, &cache);
+    
+    char* str = malloc(256);
+    strcpy(str, "I am running very far away");
+    iplus1_sentence_init(&sen, iplus1_get_lang(&iplus1, "eng"), str);
+    
+    int i;
+    printf("str: %s\n", sen.str);
+    for(i = 0; sen.words[i] != NULL; i++) {
+        printf("w: %s\n", sen.words[i]);
+    }
     
     
     
     
-    
-    
+    free(str);
+    iplus1_sentence_destroy(&sen);
     iplus1_destroy(&iplus1);
     
     return 0;
