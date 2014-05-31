e720348882cce4bcc5980d6a6981fd1b5902334c
Jake Probst <jake.probst@gmail.com>
2014-05-30 22:02:46 -0700

    avl tree implementation

---
+++ src/libiplus1/tree.c
@@ -0,0 +1,171 @@
+#include <stdlib.h>
+
+#include "iplus1.h"
+#include "tree.h"
+
+int iplus1_tree_init(iplus1_tree_t* tree, int (*compare)(void*, void*))
+{
+    tree->root = NULL;
+    tree->compare = compare;
+    
+    return IPLUS1_SUCCESS;
+}
+
+int _iplus1_tree_node_destroy(iplus1_tree_node_t* node)
+{
+    if (node == NULL)
+        return IPLUS1_FAIL;
+    _iplus1_tree_node_destroy(node->left);
+    _iplus1_tree_node_destroy(node->right);
+    free(node);
+    
+    return IPLUS1_SUCCESS;
+}
+
+int iplus1_tree_destroy(iplus1_tree_t* tree)
+{
+    _iplus1_tree_node_destroy(tree->root);
+    
+    return IPLUS1_SUCCESS;
+}
+
+int _max(int a, int b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+
+int _height(iplus1_tree_node_t* node)
+{
+    if (node == NULL)
+        return 0;
+    return node->height;
+}
+
+int _balance(iplus1_tree_node_t* node)
+{
+    if (node == NULL)
+        return 0;
+    return _height(node->left) - _height(node->right);
+}
+
+iplus1_tree_node_t* _rotate_left(iplus1_tree_node_t* node)
+{
+    iplus1_tree_node_t* tmp = node->right;
+    node->right = tmp->left;
+    tmp->left = node;
+    
+    node->height = _max(_height(node->left), _height(node->right)) + 1;
+    tmp->height = _max(_height(tmp->left), _height(tmp->right)) + 1;
+    return tmp;
+}
+
+iplus1_tree_node_t* _rotate_right(iplus1_tree_node_t* node)
+{
+    iplus1_tree_node_t* tmp = node->left;
+    node->left = tmp->right;
+    tmp->right = node;
+    
+    node->height = _max(_height(node->left), _height(node->right)) + 1;
+    tmp->height = _max(_height(tmp->left), _height(tmp->right)) + 1;
+    return tmp;
+}
+
+
+iplus1_tree_node_t* _tree_insert(iplus1_tree_node_t* root, iplus1_tree_node_t* node, int (*compare)(void*, void*))
+{
+    if (root == NULL) {
+        return node;
+    }
+    
+    int comp = compare(root->key, node->key);
+    if (comp > 0) { // root < node
+        root->left = _tree_insert(root->left, node, compare);
+    }
+    else if (comp < 0){ // root > node
+        root->right = _tree_insert(root->right, node, compare);
+    }
+    else {
+        // equal, just ignore it I guess. (this shouldnt happen)
+    }
+    
+    root->height = _max(_height(root->right), _height(root->left)) + 1;
+    int balance = _balance(root);
+    
+    if (balance > 1 && compare(root->left->key, node->key) > 0) { // root > node
+        return _rotate_right(root);
+    }
+    if (balance > 1 && compare(root->left->key, node->key) < 0) { // root < node
+        root->left = _rotate_left(root->left);
+        return _rotate_right(root);
+    }
+    if (balance < -1 && compare(root->right->key, node->key) < 0) { // root < node
+        return _rotate_left(root);
+    }
+    if (balance < -1 && compare(root->right->key, node->key) > 0) { // root < node
+        root->right = _rotate_right(root->right);
+        return _rotate_left(root);
+    }
+    
+    return root;
+}
+
+
+
+int iplus1_tree_insert(iplus1_tree_t* tree, void* key, void* value)
+{
+    iplus1_tree_node_t* node = malloc(sizeof(iplus1_tree_node_t));
+    node->key = key;
+    node->value = value;
+    node->height = 1;
+    node->left = node->right = NULL;
+    
+    tree->root = _tree_insert(tree->root, node, tree->compare);
+    
+    return IPLUS1_SUCCESS;
+}
+
+void* iplus1_tree_get(iplus1_tree_t* tree, void* key)
+{
+    iplus1_tree_node_t* cursor = tree->root;
+    for(;;) {
+        if (cursor == NULL) {
+            return NULL;
+        }
+        if (tree->compare(cursor->key, key) > 0) {
+            cursor = cursor->left;
+        }
+        else if (tree->compare(cursor->key, key) < 0) {
+            cursor = cursor->right;
+        }
+        else {
+            return cursor->value;
+        }
+    }
+    return NULL;
+}
+
+
+int _iplus1_tree_node_foreach(iplus1_tree_node_t* node, int (*func)(void*))
+{
+    
+    if (node != NULL) {
+        _iplus1_tree_node_foreach(node->left, func);
+        func(node->value);
+        _iplus1_tree_node_foreach(node->right, func);
+    }
+    return IPLUS1_SUCCESS;
+}
+
+int iplus1_tree_foreach(iplus1_tree_t* tree, int (*func)(void*))
+{
+    _iplus1_tree_node_foreach(tree->root, func);
+    return IPLUS1_SUCCESS;
+}
+
+
+
+
+
+
---
+++ src/libiplus1/tree.h
@@ -0,0 +1,46 @@
+#ifndef _TREE_H_
+#define _TREE_H_
+
+// some sort of avl tree thing that I attempted to implment after reading about
+// it on wikipedia. it is probably wrong.
+// used for reference:
+// http://www.geeksforgeeks.org/avl-tree-set-1-insertion/ 
+// http://www.eternallyconfuzzled.com/tuts/datastructures/jsw_tut_avl.aspx
+
+// no remove function cause it isn`t needed and I`m lazy
+
+typedef struct iplus1_tree_node_t {
+    void* key;
+    void* value;
+    
+    int height;
+    struct iplus1_tree_node_t* left;
+    struct iplus1_tree_node_t* right;
+} iplus1_tree_node_t;
+
+
+typedef struct iplus1_tree_t {
+    iplus1_tree_node_t* root;
+    
+    int (*compare)(void*, void*);
+} iplus1_tree_t;
+
+
+
+int iplus1_tree_init(iplus1_tree_t*, int (*compare)(void*, void*));
+int iplus1_tree_destroy(iplus1_tree_t*);
+
+
+int iplus1_tree_insert(iplus1_tree_t*, void*, void*);
+void* iplus1_tree_get(iplus1_tree_t*, void*);
+int iplus1_tree_foreach(iplus1_tree_t*, int (*func)(void*));
+
+
+
+
+
+
+
+
+
+#endif /* _TREE_H_ */
