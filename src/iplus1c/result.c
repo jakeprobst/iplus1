e7e137c12ca78ddf9a3277eb3ae432a6a4970e97
Jake Probst <jake.probst@gmail.com>
2014-06-08 04:37:00 -0700

    search result struct

---
+++ src/iplus1c/result.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+
+#include "result.h"
+
+
+
+int _free_str(void* str, void* param)
+{
+    free(str);
+    return 0;
+}
+
+
+
+
+
+int search_result_init(search_result_t* result)
+{
+    result->original = NULL;
+    result->translated = NULL;
+    result->new_words = malloc(sizeof(iplus1_list_t));
+    iplus1_list_init(result->new_words);
+    
+    return 0;
+}
+
+
+int search_result_destroy(search_result_t* result)
+{
+    free(result->original);
+    free(result->translated);
+    iplus1_list_foreach(result->new_words, &_free_str, NULL);
+    iplus1_list_destroy(result->new_words);
+    free(result->new_words);
+    
+    return 0;
+}
---
+++ src/iplus1c/result.h
@@ -0,0 +1,43 @@
+#ifndef _RESULT_H_
+#define _RESULT_H_
+
+#include "list.h"
+
+
+typedef struct search_result_t {
+    char* original;
+    char* translated;
+    iplus1_list_t* new_words;
+} search_result_t;
+
+int search_result_init(search_result_t*);
+int search_result_destroy(search_result_t*);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* _RESULT_H_ */
