1bca39992a046e8ce3a4a813794a5a2d34967204
Jake Probst <jake.probst@gmail.com>
2014-06-08 04:35:32 -0700

    preparefiles does some actual preprocessing now

--- preparefiles.py
+++ preparefiles.py
@@ -9,29 +9,63 @@ def main():
     if not os.path.exists("links.csv"):
         os.system("wget http://tatoeba.org/files/downloads/links.csv")
 
-    outfiles = {}
-
+    sentences = {}
+    #ghost = {}
+    
     senfile = open("sentences.csv")
     while True:
         line = senfile.readline()
         if line == '':
             break
-        id, lang, sen = line.split('\t')
+        line = line.strip()
+        try:
+            id, lang, sen = line.split('\t')
+        except ValueError:
+            pass # some asshole entered an empty sentence
+        sentences[id] = line
+
+
+    linkfile = open("links.csv")
+    while True:
+        line = linkfile.readline()
+        if line == '':
+            break
+        line = line.strip()
+        main_sentence, translated_sentence = line.split('\t')
+        try:
+            tr = sentences[translated_sentence]
+        except KeyError:
+            #ghost[translated_sentence] = 0
+            continue # there is a sentence that is linked but doesnt exist
         
+        try:
+            tr_id, tr_lang, tr_sen = tr.split('\t')[:3]
+        except:
+            continue
+        
+        try:
+            sentences[main_sentence] += '\t' + tr_lang + ':' + tr_sen
+        except KeyError:
+            #ghost[main_sentence] = 0
+            continue
+
+    #l = []
+    #for k in ghost:
+    #    l.append(k)
+    #l.sort()
+    #for i in l:
+    #    print i
+
+    outfiles = {}
+
+    try:
+        os.mkdir("data/")
+    except: pass
+    for k in sentences:
+        lang = sentences[k].split('\t')[1]
         if not outfiles.has_key(lang):
             outfiles[lang] = file("data/" + lang + ".csv", "w")
-
-        outfiles[lang].write(line)
-
-
-
-
-
-
-
-
-
-
+        outfiles[lang].write(sentences[k]+'\n')
 
 
 
