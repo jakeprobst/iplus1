57e6dc45072a7982b9f9ab786e9a0bb7a172108f
Jake Probst <jake.probst@gmail.com>
2014-06-10 23:28:00 -0700

    start apkg export

---
+++ src/iplus1c/apkg.c
@@ -0,0 +1,36 @@
+#include "apkg.h"
+
+
+
+
+
+
+int apkg_init(apkg_t* apkg, char* path)
+{
+    
+    
+    return 0;
+}
+
+
+int apkg_destroy(apkg_t* apkg)
+{
+    
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+int apkg_add_card(apkg_t* apkg, char* front, char* back)
+{
+    
+    return 0;
+}
