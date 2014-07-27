d9faf2dcbf28234bbd1cc88589de3b322aa2257b
Jake Probst <jake.probst@gmail.com>
2014-07-26 19:57:43 -0700

    can now select results and export to a csv

--- web/client/iplus1.js
+++ web/client/iplus1.js
@@ -8,6 +8,86 @@ var nlang = document.getElementById("nativelang");
 var tlang = document.getElementById("targetlang");
 
 
+
+iplus1.onmessage = function(event)
+{
+    switch(event.data['action']) {
+        case "result":
+            handle_result(event.data['result']);
+            break;
+        case "progress":
+            handle_progress(event.data['progress']);
+            break
+        case "apkg":
+            handle_apkg(event.data["buffer"]);
+            break;
+    }
+}
+
+function get_selected_pairs()
+{
+    var out = []
+    var results = document.getElementsByClassName("result");
+    for (var i = 0; i < results.length; i++) {
+        var div = results[i];
+        
+        console.log("checked %d %s", i, div.getElementsByTagName("input")[0].checked);
+        if (div.getElementsByTagName("input")[0].checked) {
+            var span = div.getElementsByTagName("span");
+            
+            var target = span[0].textContent;
+            var native = span[1].textContent;
+            out.push([target, native]);
+        }
+    }
+    return out;
+}
+
+
+function make_deck()
+{
+    var sents = get_selected_pairs();
+    iplus1.postMessage({action:"apkg", sentences:sents});
+}
+
+
+
+function handle_result(res)
+{
+    res['deck'].map( function (i) {
+        content.innerHTML += '<div class="result">' +
+                            '<input type="checkbox">' +
+                            "<span><strong>" + i[1] + "</strong></span><br />" +
+                            "<span>" + i[0] + "</span>" +
+                            '</div><hr />';
+    });
+    
+    var makedeck = document.createElement("input");
+    makedeck.setAttribute("type","button");
+    makedeck.setAttribute("id","makedeck");
+    makedeck.setAttribute("value","make deck");
+    content.appendChild(makedeck);
+    makedeck.addEventListener("click", make_deck, true);
+}
+
+
+function handle_progress(prog)
+{
+    content.innerHTML = prog;
+}
+
+
+function handle_apkg(buffer)
+{
+    var blob = new Blob([buffer], {type: 'text/csv'});
+    var a = document.createElement("a");
+    a.style = "display: none;";
+    document.body.appendChild(a);
+    a.href = window.URL.createObjectURL(blob);
+    a.download = "tatoeba.csv";
+    a.click();
+}
+
 function iplus1_start()
 {
     iplus1.postMessage({action: "start", file: ankifile.files[0], nativelang: nlang.value, targetlang: tlang.value});
--- web/client/worker.iplus1.js
+++ web/client/worker.iplus1.js
@@ -1,15 +1,27 @@
 importScripts("sql.js");
 
+//var content = document.getElementById("content");
+
 function clean_string(instr)
 {
     var s = instr.split("\x1f")[0];
     var out = s.replace(/{{.*?::(.*?)}}/g, "$1");
+    var out = out.replace(/(<.*?>)/g, "$1");
     return out;
 }
 
+function read_file(path)
+{
+    postMessage({action:"progress", progress:"reading file"});
+    var r = new FileReaderSync();
+    var result = r.readAsArrayBuffer(path);
+    var Uints = new Uint8Array(result);
+    return new SQL.Database(Uints);
+}
 
 function parse_database(db)
 {
+    postMessage({action:"progress", progress:"parsing db"});
     var res = db.exec("SELECT flds FROM notes;");
 
     out = [];
@@ -19,35 +31,45 @@ function parse_database(db)
     return out;    
 }
 
-function read_file(path)
+function send_request(nlang, tlang, sentences)
 {
-    var r = new FileReaderSync();
-    var result = r.readAsArrayBuffer(path);
-    var Uints = new Uint8Array(result);
-    return new SQL.Database(Uints);
+    postMessage({action:"progress", progress:"sending request"});
+    var req_data = {"native_lang":nlang, "target_lang": tlang, "deck": sentences};
+    
+    var conn = new XMLHttpRequest();
+    conn.open("POST", "/api/iplus1/?format=json", false);
+    conn.setRequestHeader("Content-Type", "application/json");
+    
+    postMessage({action:"progress", progress:"waiting for server"});
+    conn.send(JSON.stringify(req_data));
+    
+    return JSON.parse(conn.responseText);
 }
 
 function iplus1_start(path, nlang, tlang)
 {
     var db = read_file(path);
     var sentences = parse_database(db);
+    console.log(sentences);
+    var response = send_request(nlang, tlang, sentences);
     
-    var req_data = {"native_lang":nlang, "target_lang": tlang, "deck": sentences};
+    console.log(response);
+    postMessage({action:"result", result: response});
     
     
-    var conn = new XMLHttpRequest();
-    conn.open("POST", "/api/iplus1/?format=json", false);
-    conn.setRequestHeader("Content-Type", "application/json");
-    conn.send(JSON.stringify(req_data));
-    //conn.send('{"native_lang":"eng","target_lang":"jpn","deck":["…だったら尾行なんて姑息な真似、最初からしない方がよかったのに…","といっても、勿論本当に投函する訳じ ゃあ、露骨に話題を逸らした…。"]}');
     
-    console.log(conn.responseText);
-
-
 }
-
-
-
+// TODO: actually make apkg? a csv is probably better/easier
+// apkgs dont really offer anything special...
+function make_apkg(sents)
+{
+    out = []
+    sents.map( function (i) {
+        out.push(i[0] + "\t" + i[1]);
+    });
+    
+    postMessage({action:"apkg", buffer:out.join('\n')});
+}
 
 
 
@@ -56,6 +78,9 @@ function handle_message(event)
     switch(event.data['action']) {
         case "start":
             iplus1_start(event.data['file'], event.data['nativelang'], event.data['targetlang']);
+            break;
+        case "apkg":
+            make_apkg(event.data['sentences']);
             break;
     }
 }
