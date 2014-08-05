c595b00aae9786d6462ec332114c88e47c650435
Jake Probst <jake.probst@gmail.com>
2014-08-04 18:42:24 -0700

    comments in example

--- example.py
+++ example.py
@@ -12,6 +12,10 @@ NATIVE_LANG = "jpn"
 TARGET_LANG = "eng"
 sens = ["here is an example sentence", "and here is another sentence that I should fill with various words","and I should put a third sentence just in case","filter out sleep words"]
 
+# on tatoeba, sentences are kept in their own database, but for testing and 
+# local use it uses the redis server. This is why it just returns the id instead
+# of the actual sentence.
+
 results = pyplus1.parse_full(NATIVE_LANG, TARGET_LANG, sens)
 for r in results:
     n = redisdb.hget(NATIVE_LANG + '-full', r['nid'])
